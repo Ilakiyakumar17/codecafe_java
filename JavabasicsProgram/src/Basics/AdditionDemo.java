adfb339e4fbd8cd9325b985735f9ccd9a24ae69b
ilaki <ilaki@DELL>
2024-05-23 22:06:20 +0530

    This project is reference for a Java Basics

---
+++ JavabasicsProgram/src/Basics/AdditionDemo.java
@@ -0,0 +1,19 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("addition of a="+a+"and b="+"is:"+c);
+
+	}
+
+}
