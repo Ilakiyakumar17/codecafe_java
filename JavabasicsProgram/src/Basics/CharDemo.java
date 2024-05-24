7dc3d5934dd4575bd27913823e43ed8860595463
ilaki <ilaki@DELL>
2024-05-24 08:16:35 +0530

    This project is reference for a JavaBasics

---
+++ JavabasicsProgram/src/Basics/CharDemo.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		
+		char ch = 'a';
+		System.out.println(ch);
+		 
+		char ch1 = 65;
+		System.out.println(ch1);
+		
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		
+		char var2 = '\u20AC';
+		System.out.println(var2);
+		
+		int a = 'A';
+		System.out.println(a);
+		
+	}
+
+}
