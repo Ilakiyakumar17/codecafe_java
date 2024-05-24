e33d0a02a17fbbbbd0fab0cdaabda92cb62f25d9
ilaki <ilaki@DELL>
2024-05-24 08:58:12 +0530

    This project is reference for a Java Basics

---
+++ JavabasicsProgram/src/Basics/DataTypeDemo.java
@@ -0,0 +1,22 @@
+package Basics;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		
+		int value1 = 9/2;
+		float value2 = 101/61;
+		double value3=10d/6d;
+		float value4 = 5/2.5f;
+		System.out.println("value 1="+value1);
+		System.out.println("value 2="+value2);
+		System.out.println("value 3="+value3);
+		System.out.println("value 4="+value4);
+		
+		int marker = 512;
+		double percentage = marker*0.46f;
+		System.out.println("percentage:"+percentage);
+
+	}
+
+}
---
+++ JavabasicsProgram/src/Basics/IdentifiersDemo.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+	
+		int $number = 20;
+		System.out.println("value of the number variable is:"+$number);
+		String studentName="ilakiya";
+		System.out.println("value is :"+studentName);
+
+	}
+
+}
